3b2c3318c3736ec77d728cf3a32153f1dff6e46e
Nivid Shah <shahn8@student.douglascollege.ca>
2022-04-03 23:05:00 -0700

    result Image displayed in webapp

--- WebApp/src.js
+++ WebApp/src.js
@@ -21,13 +21,13 @@
        let file = document.getElementById("fileupload");
        var formData = new FormData();
        formData.append("image",file.files[0])
-       axios.post('http://localhost:5000/', formData, {
+       axios.post('http://127.0.0.1:5000/', formData, {
            headers: {
                'Content-Type': 'multipart/form-data'
            }
        }).then((data) => {
-           console.log(data);
-           document.getElementById('resultImage').src = 'data:image/png;,' + btoa(data);
+           //console.log(data);
+           document.getElementById('resultImage').src = 'data:image/gif;base64,' + data.data;
        });
 
       

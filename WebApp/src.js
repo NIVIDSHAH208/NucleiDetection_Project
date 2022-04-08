eb9cdbfd0138ed2a97a410337b8685b8cd524ac9
Nivid Shah <shahn8@student.douglascollege.ca>
2022-04-08 02:05:42 -0700

    Final Project

--- WebApp/src.js
+++ WebApp/src.js
@@ -1,19 +1,3 @@
-//    mainForm.onsubmit = async (e) => {
-//     e.preventDefault();
-//        let file = document.getElementById("fileupload");
-//        var formData = new FormData();
-//        formData.append("image",file.files[0])
-//     let response = await fetch('http://127.0.0.1:5000/', {
-//       method: 'POST',
-//         body: formData,
-      
-//     });
-
-//        let result = await response;
-//        //window.location.href(result);
-
-   
-//   };
 
 
    mainForm.onsubmit = async (e) => {
@@ -30,10 +14,7 @@
            document.getElementById('resultImage').src = 'data:image/gif;base64,' + data.data;
        });
 
-      
-      
 
-//        //window.location.href(result);
 
    
   };
